f59a5bdeecc3196a9ec7ac4f2c6d9885d9f45827
Julio Cesar Oliveira da Silva <julioghost1@gmail.com>
2024-09-06 12:14:21 -0300

    feat:the-movie-db-api - created provider API to search Movies and created useCase to get movies

--- src/app/Domain/UseCases/Movies/GetMoviesUseCase.php
+++ src/app/Domain/UseCases/Movies/GetMoviesUseCase.php
@@ -2,12 +2,17 @@
 
 namespace App\Domain\UseCases\Movies;
 
+use App\Infrastructure\Providers\TheMovieDbApiProvider;
+
 class GetMoviesUseCase
 {
 
+    public function __construct(protected TheMovieDbApiProvider $theMovieDB) {}
+
     public function execute(array $data): array
     {
+        $movies = $this->theMovieDB->searchMovies($data['query'], $data['page']);
 
-        return [];
+        return $movies;
     }
 }
