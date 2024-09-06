36936152caa9e6c00ab39874a9429215f8c49c2c
Julio Cesar Oliveira da Silva <julioghost1@gmail.com>
2024-09-06 12:42:25 -0300

    feat:the-movie-db-api - added padronization in movies return

--- src/app/Infrastructure/Providers/TheMovieDbApiProvider.php
+++ src/app/Infrastructure/Providers/TheMovieDbApiProvider.php
@@ -26,21 +26,30 @@ class TheMovieDbApiProvider
     public function searchMovies(string $query, int $page = 1): array
     {
         try {
-            $response = $this->client->withHeaders([
+            $response = $this->client::withHeaders([
                 'Authorization' => 'Bearer ' . $this->apiKey,
                 'accept' => 'application/json',
             ])->get(self::BASE_URL . 'search/movie', [
                 'query' => $query,
                 'page' => $page,
                 'include_adult' => false,
-                'language' => 'en-US',
+                'language' => 'pt-BR',
             ]);
 
             if ($response->failed()) {
                 throw new Exception($response->body());
             }
 
-            return $response->json();
+            $reusult = $response->json();
+
+            return [
+                'movies' => $reusult['results'],
+                'page' => [
+                    'total' => $reusult['total_pages'],
+                    'current' => $reusult['page'],
+                    'total_movies' => $reusult['total_results'],
+                ]
+            ];
         } catch (Exception $e) {
             throw new Exception("Error retrieving movies from The Movie DB: " . $e->getMessage());
         }
