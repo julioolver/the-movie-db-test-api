6e1b372a5b51267762d0262f4294187895f2b989
Julio Cesar Oliveira da Silva <julioghost1@gmail.com>
2024-09-06 12:45:52 -0300

    feat:the-movie-db-api  #2 - added movie router and adjusted controller to return json movies

--- src/app/Http/Controllers/Api/MovieController.php
+++ src/app/Http/Controllers/Api/MovieController.php
@@ -2,17 +2,32 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Domain\UseCases\Movies\GetMoviesUseCase;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Client\Response;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response as HttpResponse;
+use Illuminate\Support\Facades\Http;
 
 class MovieController extends Controller
 {
+    public function __construct(protected GetMoviesUseCase $getMoviesUseCase) {}
+
     public function index(Request $request): JsonResponse
     {
-        $query = $request->input('query', '');
-        $page = $request->input('page', 1);
+        try {
+            $query = $request->input('query', '');
+            $page = $request->input('page', 1);
 
-        return response()->json();
+            $movies = $this->getMoviesUseCase->execute([
+                'query' => $query,
+                'page' => $page
+            ]);
+
+            return response()->json($movies);
+        } catch (\Throwable $th) {
+            return response()->json(['message' => $th->getMessage()], HttpResponse::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
