039325a5119c7feea91e9e732b1e11fd2cd3178d
Julio Cesar Oliveira da Silva <julioghost1@gmail.com>
2024-09-07 15:44:41 -0300

    feat:the-movie-db-api  #5 - created StoreMovieRequest to validate fields and added method in Controller

---
+++ src/app/Http/Requests/Movie/StoreMovieRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\Movie;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreMovieRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'external_id' => ['required', 'string', 'max:255'],
+            'provider' => ['required', 'string', 'max:255'],
+            'title' => ['required', 'string', 'max:255'],
+            'director' => ['nullable', 'string', 'max:255'],
+            'synopsis' => ['nullable', 'string', 'max:255'],
+            'duration' => ['nullable', 'string', 'max:10'],
+            'year' => ['nullable', 'string', 'max:4'],
+            'rating' => ['nullable', 'string', 'max:255'],
+            'poster_path' => ['nullable', 'string', 'max:255'],
+            'is_watched' => ['required', 'boolean'],
+            'is_favorite' => ['required', 'boolean'],
+            'watch_later' => ['required', 'boolean'],
+        ];
+    }
+}
