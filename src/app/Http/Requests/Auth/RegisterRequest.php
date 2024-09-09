7fde46c445acc8d27f9cc3cc8b653b86f695a1d9
Julio Cesar Oliveira da Silva <julioghost1@gmail.com>
2024-09-09 14:32:29 -0300

    added password_confirmation

--- src/app/Http/Requests/Auth/RegisterRequest.php
+++ src/app/Http/Requests/Auth/RegisterRequest.php
@@ -24,7 +24,7 @@ class RegisterRequest extends FormRequest
         return [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:5'],
+            'password' => ['required', 'string', 'confirmed', 'min:5'],
         ];
     }
 }
