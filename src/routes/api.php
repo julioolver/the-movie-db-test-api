bdf257760f68f486f2c6d829f39fb741daa928c4
Julio Cesar Oliveira da Silva <julioghost1@gmail.com>
2024-09-09 16:41:52 -0300

    adjusted api

--- src/routes/api.php
+++ src/routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\MovieController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
@@ -22,8 +23,13 @@ Route::group([
 ], function () {
     Route::get('/', [MovieController::class, 'index']);
     Route::post('/', [MovieController::class, 'store']);
-
     Route::put('{movie}/status', [MovieController::class, 'updateStatus']);
     Route::get('{id}', [MovieController::class, 'getMovieDetails']);
-    Route::get('user/movies', [MovieController::class, 'getUserMovies']);
+});
+
+Route::group([
+    'middleware' => ['api', 'auth:api'],
+    'prefix' => 'user'
+], function () {
+    Route::get('movies', [MovieController::class, 'getUserMovies']);
 });
